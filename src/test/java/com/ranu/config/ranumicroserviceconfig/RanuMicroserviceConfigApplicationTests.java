57ce0b9d37e7905a2df221a2ea5ea0d6ac8956cc
unknown <snlrana65@gmail.com>
2020-09-01 12:45:26 +0530

    first commit

---
+++ src/test/java/com/ranu/config/ranumicroserviceconfig/RanuMicroserviceConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ranu.config.ranumicroserviceconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RanuMicroserviceConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
